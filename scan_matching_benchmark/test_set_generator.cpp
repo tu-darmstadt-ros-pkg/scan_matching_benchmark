eab28be3d2d972319423803301ad4175a1a0eedc
Kevin Daun <kdaun@outlook.de>
2017-09-14 17:58:36 +0200

    Added measurement noise for data generation

--- scan_matching_benchmark/test_set_generator.cpp
+++ scan_matching_benchmark/test_set_generator.cpp
@@ -39,6 +39,10 @@ void TestSetGenerator::generateCube(cartographer::sensor::PointCloud& cloud, flo
 }
 
 void TestSetGenerator::generateCuboid(cartographer::sensor::PointCloud& cloud, float size_x, float size_y, float size_z) {
+  std::random_device r;
+  std::default_random_engine e1(r());
+  std::normal_distribution<float> normal_distribution(0, 0.01);
+
   cloud.clear();
   float min_x = -size_x / 2.0;
   float max_x = size_x / 2.0;
@@ -50,26 +54,26 @@ void TestSetGenerator::generateCuboid(cartographer::sensor::PointCloud& cloud, f
   for(float x = min_x; x <= max_x; x += resolution_) {
     for(float y = min_y; y <= max_y; y += resolution_) {
       float z = min_z;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
       z = max_z;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
     }
   }
   for(float x = min_x; x <= max_x; x += resolution_) {
     for(float z = min_z; z <= max_z; z += resolution_) {
       float y = min_y;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
       y = max_y;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
     }
   }
 
   for(float y = min_y; y <= max_y; y += resolution_) {
     for(float z = min_z; z <= max_z; z += resolution_) {
       float x = min_x;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
       x = max_x;
-      cloud.emplace_back(Eigen::Vector3f(x, y, z));
+      cloud.emplace_back(Eigen::Vector3f(x + normal_distribution(e1), y + normal_distribution(e1), z + normal_distribution(e1)));
     }
   }
 }
